48903e610b45c4801e17b43b280c7d5af18a3e31
Arsenii55 <arseka2512@gmail.com>
2023-08-27 11:23:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+       int x =567;
+       long h = 1999;
+       double c = 55.6;
+       float f = 33.5f;
+       String bob = "hello me Bob";
+       String l = " ";
+       String last = "nice to meet you";
+        System.out.println(bob+l+last);
+        int i = 0;
+        int io = 2;
+        while(i<io){
+            System.out.println("io>");
+            i++;
+        }
+        System.out.println("Мы вышли из цикла");
+    for (int p = 0;p<10;p++){
+        System.out.println("10>p");
+    }
+        System.out.println("p>10");
+    
+    }
+}
